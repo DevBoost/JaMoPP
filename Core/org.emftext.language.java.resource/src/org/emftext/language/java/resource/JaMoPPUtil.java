9aeacf3f00fe0fd2ca93d029b8030a4d572d86b6
Mirko Seifert <mirko.seifert@devboost.de>
2014-06-11 21:50:39 +0200

    Added documentation Revised code

--- Core/org.emftext.language.java.resource/src/org/emftext/language/java/resource/JaMoPPUtil.java
+++ Core/org.emftext.language.java.resource/src/org/emftext/language/java/resource/JaMoPPUtil.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006-2012
+ * Copyright (c) 2006-2014
  * Software Technology Group, Dresden University of Technology
  * DevBoost GmbH, Berlin, Amtsgericht Charlottenburg, HRB 140026
  * 
@@ -19,14 +19,24 @@ import java.util.Map;
 
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.Resource.Factory;
+import org.eclipse.emf.ecore.resource.Resource.Factory.Registry;
 import org.emftext.language.java.JavaPackage;
 
 public class JaMoPPUtil {
 
+	/**
+	 * Globally registers the JaMoPP resource factories for Java source and
+	 * class files. Also, the JaMoPP meta model for Java is added to the EMF
+	 * package registry.
+	 */
 	public static void initialize() {
 		EPackage.Registry.INSTANCE.put(JavaPackage.eNS_URI, JavaPackage.eINSTANCE);
-		Map<String, Object> extensionToFactoryMap = Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap();
-		extensionToFactoryMap.put("java", new JavaSourceOrClassFileResourceFactoryImpl());
-		extensionToFactoryMap.put("class", new JavaSourceOrClassFileResourceFactoryImpl()); 
+		
+		Registry registry = Resource.Factory.Registry.INSTANCE;
+		Map<String, Object> extensionToFactoryMap = registry.getExtensionToFactoryMap();
+		Factory factory = new JavaSourceOrClassFileResourceFactoryImpl();
+		extensionToFactoryMap.put("java", factory);
+		extensionToFactoryMap.put("class", factory); 
 	}
 }
