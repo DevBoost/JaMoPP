279b04ca287a0b25d910b5ece20a555824bd07e3
mseifert <mirko.seifert@devboost.de>
2015-04-12 20:25:22 +0200

    Updated copyright header

--- Core/org.emftext.language.java/src/org/emftext/language/java/JavaClasspath.java
+++ Core/org.emftext.language.java/src/org/emftext/language/java/JavaClasspath.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
- * Copyright (c) 2006-2014
+ * Copyright (c) 2006-2015
  * Software Technology Group, Dresden University of Technology
- * DevBoost GmbH, Berlin, Amtsgericht Charlottenburg, HRB 140026
+ * DevBoost GmbH, Dresden, Amtsgericht Dresden, HRB 34001
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -10,7 +10,7 @@
  * 
  * Contributors:
  *   Software Technology Group - TU Dresden, Germany;
- *   DevBoost GmbH - Berlin, Germany
+ *   DevBoost GmbH - Dresden, Germany
  *      - initial API and implementation
  ******************************************************************************/
 package org.emftext.language.java;
