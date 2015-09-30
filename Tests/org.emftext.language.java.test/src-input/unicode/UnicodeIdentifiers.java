b325992cc4e5367ca1a4b55745bc854eeb54b8c0
mseifert <mirko.seifert@devboost.de>
2015-09-30 13:19:15 +0200

    Modified identifiers to compile code with Java 7

--- Tests/org.emftext.language.java.test/src-input/unicode/UnicodeIdentifiers.java
+++ Tests/org.emftext.language.java.test/src-input/unicode/UnicodeIdentifiers.java
@@ -16,13 +16,13 @@
 package unicode;
 
 public class UnicodeIdentifiers {
-	int i\u0000 = 0;
-	int i\u202a;
+	int a\u0000 = 0;
+	int b\u202a;
     int \u0041; // LATIN CAPITAL A
     int \u0061; // LATIN SMALL A
     int \u0391; // GREEK CAPITAL ALPHA
     int \u0480; // CYRILLIC SMALL A
     int \u00c1; // LATIN CAPITAL A ACUTE
     int \u0041\u0301; // LATIN CAPITAL A, followed by NON-SPACING ACUTE
-    int i\u001a = 0;
+    int c\u001a = 0;
 }
